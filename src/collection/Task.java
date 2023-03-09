4a5200702d1d48f6ca466dc7893ac897a2b8e566
hiroki-coffee <125346034+hiroki-coffee@users.noreply.github.com>
2023-03-09 21:05:21 +0900

    課題再提出

--- src/collection/Task.java
+++ src/collection/Task.java
@@ -2,14 +2,6 @@ package collection;
 
 import java.time.LocalDate;
 
-/*ソートについては、外部サイトで恐縮ですが、以下のサイトを参考にしてみてください！
-https://pointsandlines.jp/server-side/java/list-sort-comparable
-
-端的に言いますと、TaskクラスでComparableインターフェースを継承し、compareToメソッドを実装します。
-このTaskクラスを含むListであれば、Collections.sort(list)ができるようになります。
-
-compareToメソッドを実装してはじめて、sortを実行できるということですね！*/
-
 public class Task implements Comparable<Task> {
 	private LocalDate date;
 	private String task;
